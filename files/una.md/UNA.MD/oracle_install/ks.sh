10b5cbd0426da472f79bb9e9862489c1b6aac5e9
Serghei Talpa <sergheitalpa@gmail.com>
2017-12-08 09:50:37 -0500

    fixed bugs

--- files/una.md/UNA.MD/oracle_install/ks.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-mkdir /temp1
-mkdir /temp2
-mkdir /temp3
-mkdir /temp4
-mkdir /temp5
-mkdir /temp6
--- files/una.md/UNA.MD/oracle_install/oracle_pre.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-#sh ./oracle_yum.sh
-
-DISPLAY=:0 
-export DISPLAY
-
-#Обновляем
-#yum update -y
